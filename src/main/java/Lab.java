bfe9e59c2892b0963474db46537f6f70bd2547ac
Desaneedidurgavasantha <dedurga132@gmail.com>
2025-05-14 17:52:26 +0530

    DONE

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -55,7 +55,7 @@ public class Lab {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
-        return a<=b;
+      return a<=b;
     }
     /**
      * @param a the first number to be compared.
